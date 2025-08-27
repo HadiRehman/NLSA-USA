fb75c3e8eec6fe0f3348d3bdeeecdd05352051b1
HadiRehman <shaikhhadi014@gmail.com>
2025-08-28 02:51:18 +0500

    setup routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import AdminDashboard from "./pages/AdminDashboard";
+import Cookies from "js-cookie";
+import Login from "./pages/Login";
+import UpdateProfile from "./pages/UpdateProfile";
+import ShowUsers from "./pages/ShowUsers";
+import AddUser from "./pages/AddUser";
+import DeleteUser from "./pages/DeleteUser";
 
 function App() {
+  const getUsername = Cookies.get("username");
+  const getRole = Cookies.get("user_role");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {getUsername ?(
+        <Routes>
+          <Route path="/" element={<AdminDashboard />}></Route>
+          <Route path="/updateprofile" element={<UpdateProfile />}></Route>
+          {getRole === "Administrator" &&
+            <>
+              <Route path="/users" element={<ShowUsers />}></Route>
+              <Route path="/adduser" element={<AddUser />}></Route>
+              <Route path="/deleteuser/:id" element={<DeleteUser />}></Route>
+            </>
+          }
+        </Routes>
+      ):(
+        <Login />
+      )
+      }
     </div>
   );
 }
