8d7595078f4e604ee051b5e1a96f357715253812
HadiRehman <shaikhhadi014@gmail.com>
2025-08-28 23:16:24 +0500

    change Login to App

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -98,7 +98,7 @@ export default function Sidebar(){
             </div>    
             ):
             (
-                <Login />
+                <App />
             )
             }
         </div>
